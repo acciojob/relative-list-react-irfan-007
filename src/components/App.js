7068d6927e9b624904c6d1baaaf108cfac6a9448
irfan-007 <thanatos786687@gmail.com>
2024-09-24 20:03:21 +0530

    done 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [list, setList] = useState([
+    "aunty 1",
+    "aunty 2",
+    "uncle 1",
+    "uncle 2",
+    "uncle 3",
+  ]);
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {list.map((item, i) => (
+          <li key={`relativeListItem${i + 1}`}>{item}</li>
+        ))}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
